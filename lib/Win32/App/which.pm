47a7ca3f53522ecbf2fa8ff4056887f0b3efe34f
Olivier Mengué <dolmen@cpan.org>
2011-03-13 22:55:25 +0100

    Add minimalist documentation.

--- lib/Win32/App/which.pm
+++ lib/Win32/App/which.pm
@@ -4,3 +4,21 @@ package Win32::App::which;
 our $VERSION = '0.01';
 
 1;
+
+=head1 NAME
+
+Win32::App::which - The 'which' Unix classic command for Windows
+
+=head1 SYNOPSIS
+
+See L<which>.
+
+=head1 AUTHOR
+
+Olivier MenguE<eacute> <dolmen@cpan.org>.
+
+=head1 LICENSE
+
+Same as Perl 5.
+
+=cut
