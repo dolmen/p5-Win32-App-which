d07d47ae5e26af96a6a1f1830419b8097e522b4d
Olivier Mengué <dolmen@cpan.org>
2011-03-18 21:11:45 +0100

    which.cmd: major refactoring (WIP)
    Work in progress:
    - start '-a' support (WIP)
    - add '--' support (à la POSIX)
    - refactoring in progress
    Still have to fix order in which files are listed.

--- bin/which.cmd
+++ bin/which.cmd
@@ -1,24 +1,44 @@
 @echo off
-:: Copyright: -© 2010 Olivier Mengu+©
+:: Copyright © 2010-2011 Olivier Mengu‚
 :: License: GPLv3
 
 if "%1"=="" exit /B 1
-if not "%~x1"=="" if exist "%1" echo %~f1& goto :EOF
-setlocal
-set F=%~nx1
-call :$PathFinder P "%F%"
-call :Found?
-for %%e in (%PATHEXT%) do (
-    call :$PathFinder P "%F%%%e"
-    call :Found?
-)
 
+setlocal
+set SPEC=First
+if "%1"=="-a" (
+    set SPEC=All
+    shift
+)
+if "%1"=="--" shift
+
+:: Add the current directory to the PATH
+call :$AbsolutePath P .
+set PATH=%P%;%PATH%
+
+goto :Find%SPEC%
+
+:FindFirst
+if "%~x1"=="" goto :NoExt
+
+:: Extension provided
+if exist "%~1" echo %~f1& goto :EOF
+call :Find "%~1"
+goto :End
+
+:NoExt
+set F=%~1
+for %%e in (%PATHEXT%) do call :Find "%F%%%e"
+
+:End
 if "%Found%"=="1" goto :OK
-echo %F% not found.>&2
+echo %~1 not found.>&2
 endlocal
 exit /B 1
 
-:Found?
+:Find
+::echo.%~1
+call :$PathFinder P "%~1"
 if "%P%"=="" goto :EOF
 if "%~f1"=="" call :Found?2 "%P%" & goto :EOF
 :Found?2
@@ -27,6 +47,11 @@ set Found=1
 echo.%P%
 goto :EOF
 
+:FindAll
+echo Not yet implemented.>&2
+exit /B 1
+
+
 :OK
 endlocal
 exit /B 0
@@ -34,3 +59,8 @@ exit /B 0
 :$PathFinder
 set "%1=%~$PATH:2"
 goto :EOF
+
+:$AbsolutePath
+set "%1=%~f2"
+goto :EOF
+
