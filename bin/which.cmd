ebd5dee465081e710029dc203631baace64f5e00
Olivier Mengué <dolmen@cpan.org>
2011-03-19 01:39:34 +0100

    Major which.cmd refactoring for speed.
    Major refactoring for optimizations.
    Also restored the shortcut for the common case to use the CMD.exe built-in facilities.

--- bin/which.cmd
+++ bin/which.cmd
@@ -2,15 +2,15 @@
 :: Copyright © 2010-2011 Olivier Mengu‚
 :: License: GPLv3
 
-if "%~1"=="" exit /B 1
-
 setlocal
 set SPEC=First
 if "%~1"=="-a" (
     set SPEC=All
     shift
 )
-if "%1"=="--" shift
+if "%~1"=="--" shift
+
+if "%~1"=="" exit /B 2
 
 :: Add the current directory to the PATH
 call :$AbsolutePath P .
@@ -18,40 +18,47 @@ set PATH=%P%;%PATH%
 
 set Found=
 
-if not "%~x1"=="" set F=%~1& call :VisitPATH :Visit%SPEC%Ext
-set F=%~1
-if "%Found%"=="1" exit /B 0
-call :VisitPATH :Visit%SPEC%
-endlocal & if "%Found%"=="1" exit /B 0
+:: Shortcut for this special case that the shell knows how to handle
+if %SPEC%==First if not "%~x1"=="" goto :FirstExtBuiltin
+
+::Slow
+if not "%~x1"=="" call :VisitPATH :Visit%SPEC%Ext "%~1"
+if not "%~x1"=="" (
+    if not #%Found%==#1 call :VisitPATH :Visit%SPEC% "%~n1"
+) else (
+    call :VisitPATH :Visit%SPEC% "%~1"
+)
+:End
+endlocal & if #%Found%==#1 exit /B 0
 echo %~1 not found.>&2
-endlocal
 exit /B 1
 
+:: Use builtin for this special case
+:FirstExtBuiltin
+set Found=%~$PATH:1
+if not "%Found%"=="" echo %Found%& set Found=1
+goto :End
+
+:: Test one filename
+:VisitFirstExt2
+if #%Found%==#1 goto :EOF
 :VisitFirstExt
-if "%Found%"=="1" goto :EOF
-if not exist "%~1\%F%" goto :EOF
-echo %~1\%F%
-set Found=1
-goto :EOF
-
-:VisitFirst
-if "%Found%"=="1" goto :EOF
-set G=%F%
-for %%e in (%PATHEXT%) do set F=%G%%%e& call :VisitFirstExt "%~1"
-set F=%G%
-goto :EOF
-::if not "%Found%"=="1" if exist "%~1\%F%%%e" echo %~1\%F%%%e& set Found=1
-
 :VisitAllExt
-if not exist "%~1\%F%" goto :EOF
-echo %~1\%F%
+:VisitAllExt1
+:VisitAllExt2
+::echo %~1 ?
+if not exist "%~1" goto :EOF
+echo %~f1
 set Found=1
 goto :EOF
 
+:: Test all extensions for the %1 filename
+:VisitFirst
 :VisitAll
-for %%e in (%PATHEXT%) do (
-    if exist "%~1\%F%%%e" echo %~1\%F%%%e& set Found=1
-)
+:VisitAll1
+for %%e in (%PATHEXT%) do call :Visit%SPEC%Ext2 "%~1%%e"
+:VisitFirst1
+:VisitFirstExt1
 goto :EOF
 
 :$AbsolutePath
@@ -64,13 +71,14 @@ goto :EOF
 ::
 : == VisitPATH ==
 set VisitPATH_P=%PATH%
-call :VisitPATH_Loop %1 .
+call :VisitPATH_Loop . %1 "%~2"
 goto :EOF
 
 :VisitPATH_Loop
 set VisitPATH_Q=%PATH%
-:: Find the first "." in the reduced %PATH% => first directory in %PATH%
-set PATH=%VisitPATH_P%& call %1 "%~$PATH:2"
+:: Find the first "." (%1) in the reduced %PATH% => first directory in %PATH%
+set PATH=%VisitPATH_P%& call %2%Found% "%~$PATH:1\%~3"
+:: Delete the first path
 set PATH=%VisitPATH_Q:*;=%
 if not "%PATH%"=="%VisitPATH_Q%" goto :VisitPATH_Loop
 
